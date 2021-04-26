c7dffa3c219ac46ea5881f24a045f42111d45289
juanPcEscritorio <juansartor92@gmail.com>
2021-04-25 21:02:55 -0300

    Complete los escenarios faltantes

--- tp/src/frsf/cidisi/faia/exercise/search/caperucita/CaperucitaEnvironmentState.java
+++ tp/src/frsf/cidisi/faia/exercise/search/caperucita/CaperucitaEnvironmentState.java
@@ -253,10 +253,265 @@ public class CaperucitaEnvironmentState extends EnvironmentState {
 			
 			
 			
-		case 2: //escenario 2
+		case 2:
+			//escenario 2
+			
+			
+			//ARBOLES
+	        mapa[0][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][6] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[1][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        
+	        
+	        
+	        mapa[2][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[3][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        
+	        mapa[4][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[5][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[6][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	      
+	        
+	        mapa[7][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[8][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        //DULCES 
+	        mapa[1][5] = CaperucitaPerception.DULCE_PERCEPTION;
+	        mapa[3][8] = CaperucitaPerception.DULCE_PERCEPTION;
+	        mapa[4][10] = CaperucitaPerception.DULCE_PERCEPTION;
+
+	        //CAMPO DE FLORES
+	        mapa[7][6] = CaperucitaPerception.FLORES_PERCEPTION;
+	        mapa[8][6] = CaperucitaPerception.FLORES_PERCEPTION;
+	     
+
+	        //LOBO
+	        mapa[7][10] = CaperucitaPerception.LOBO_PERCEPTION; //Posicion inicial del lobo la seteamos? luego random
+	        
+	        //CAPERUCITA
+	        this.setPosicionAgente(new int[]{6, 3});
+				
+			
+			
+			
+			
+	
+			
+			
 			
 			
 		case 3: //escenario 3
+			
+			
+			
+			//ARBOLES
+	        mapa[0][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][6] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[0][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[1][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[1][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        
+	        mapa[2][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][6] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[2][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[3][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[3][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        
+	        mapa[4][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[4][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[5][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[5][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[6][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[6][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	      
+	        
+	        mapa[7][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[7][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        mapa[8][0] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][1] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][2] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][3] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][4] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][5] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][6] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][7] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][8] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][9] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][10] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][11] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][12] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        mapa[8][13] = CaperucitaPerception.ARBOL_PERCEPTION;
+	        
+	        //DULCES 
+	        mapa[2][7] = CaperucitaPerception.DULCE_PERCEPTION;
+	        mapa[6][4] = CaperucitaPerception.DULCE_PERCEPTION;
+	        mapa[6][8] = CaperucitaPerception.DULCE_PERCEPTION;
+
+	        //CAMPO DE FLORES
+	        mapa[0][3] = CaperucitaPerception.FLORES_PERCEPTION;
+	        mapa[1][3] = CaperucitaPerception.FLORES_PERCEPTION;
+	     
+
+	        //LOBO
+	        mapa[7][6] = CaperucitaPerception.LOBO_PERCEPTION; //Posicion inicial del lobo la seteamos? luego random
+	        
+	        //CAPERUCITA
+	        this.setPosicionAgente(new int[]{4, 11});
+				
+			
+					
 			
 		
 		}
